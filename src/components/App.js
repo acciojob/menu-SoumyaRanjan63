4a09f4a8261470317d3e4e4407a8671c89eeb975
SoumyaRanjan63 <sranjan6370@gmail.com>
2023-10-09 22:12:57 +0530

    first Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import MainContainer from './MainContainer';
 
 const App=()=>{
     return (<div>
     
-     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+     {/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
+     <MainContainer />
     </div>
     )
 }
---
+++ src/components/MainContainer.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import { dishes } from './dishes';
+
+const MainContainer = () => {
+    const [currentCategory,setCurrentCatagory]=useState('');
+
+    const handleButtonClick=(category)=>{
+        setCurrentCatagory(category);
+    }
+    
+
+    const renderDishes=()=>{
+        const dishList=
+        currentCategory==='All'?dishes:
+        dishes.filter((dish)=>dish.category===currentCategory);
+
+        return dishList.map((dish)=>(
+            <div key={dish.id}>
+                <img src={dish.img} alt={dish.title}/>
+                <h1>{dish.title}</h1>
+                <p>{dish.price}</p>
+                <p>{dish.desc}</p>
+            </div>
+        ));
+    };
+   
+
+    return (
+        <div>
+            <div className='navbar'>
+                <button onClick={()=>handleButtonClick('All')}>All</button>           
+                <button onClick={()=>handleButtonClick('breakfast')}>Breakfast</button>
+                <button onClick={()=>handleButtonClick('lunch')}>Lunch</button>
+                <button onClick={()=>handleButtonClick('shakes')}>Shakes</button>
+            </div>
+
+            <div className='dishes'>
+               {renderDishes()}
+            </div>
+        </div>
+    );
+}
+
+export default MainContainer;
